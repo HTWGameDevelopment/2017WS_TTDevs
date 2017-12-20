e63c8afb78b02e04ab4052f7146d2c004c8335fe
Roman <rberezin@gmx.de>
2017-12-20 20:39:53 +0100

    Factory eingebaut

---
+++ ToJ_02/Assets/CreepReader.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class CreepReader : MonoBehaviour {
+    public Transform sphereCreep;
+    public Transform cubeCreep;
+    public Transform cylinderCreep;
+    public Transform spawnPoint;
+
+
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(readLevel("Levels/TestFile.txt"));
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+    }
+
+    IEnumerator readLevel(string filename)
+    {
+        using (StreamReader sr = new StreamReader(filename))
+        {
+            string line = "";
+
+            while (line != null)
+            {
+                line = sr.ReadLine();
+                String[] substrings = line.Split(':'); 
+                //printArray(substrings);
+                int type;
+                int amount;
+                Int32.TryParse(substrings[0], out type);
+                Int32.TryParse(substrings[1], out amount);
+                StartCoroutine(spawnThem(type, amount, 0, 0));
+                yield return new WaitForSeconds(4f);
+
+            }
+            sr.Close();
+
+        }
+    }
+
+
+
+
+    
+
+    void printArray(String[] array)
+    {
+        for (int i = 0; i <= array.Length - 1; i++)
+        {
+            print(array[i]);
+        }
+    }
+
+    IEnumerator spawnThem(int type, int amount, int health, float speed)
+    {
+        switch (type)
+        {
+            case 0:
+                for (int i = 0; i <= amount; i++)
+                {
+                    Instantiate(sphereCreep, spawnPoint.position, spawnPoint.rotation);
+                    yield return new WaitForSeconds(0.2f);
+                }
+                break;
+
+            case 1:
+                for (int i = 0; i <= amount; i++)
+                {
+                    Instantiate(cubeCreep, spawnPoint.position, spawnPoint.rotation);
+                    yield return new WaitForSeconds(0.2f);
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i <= amount; i++)
+                {
+                    Instantiate(cylinderCreep, spawnPoint.position, spawnPoint.rotation);
+                    yield return new WaitForSeconds(0.2f);
+                }
+                break;
+        }
+    }
+}
--- ToJ_02/Assets/Scripts/Creeps.cs
+++ ToJ_02/Assets/Scripts/Creeps.cs
@@ -2,28 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Creeps : MonoBehaviour {
+public class Creeps : MonoBehaviour
+{
 
-	private int health = 100;
-	private int rstnc_fire = 0;
-	private int rstnc_water = 0;
-	private int rstnc_wind = 0;
-	private static float speed = 10f;
-	private int creepType = 0;
+    public static int health = 100;
+    public static int rstnc_fire = 0;
+    public static int rstnc_water = 0;
+    public static int rstnc_wind = 0;
+    public static float speed = 10f;
+    public int creepType = 0;
 
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	public static float getSpeed() {
-		return speed;
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static float getSpeed()
+    {
+        return speed;
+    }
+
+    public static int getHealth()
+    {
+        return health;
+    }
+
+    public static int getFire()
+    {
+        return rstnc_fire;
+    }
+
+    public static int getWater()
+    {
+        return rstnc_water;
+    }
+
+    public static int getWind()
+    {
+        return rstnc_wind;
+    }
+
+    public static void setHealth(int value)
+    {
+        health = value;
+    }
+
+    public static void setFire(int value)
+    {
+        rstnc_fire = value;
+    }
+
+    public static void setWater(int value)
+    {
+        rstnc_water = value;
+    }
+    public static void setWind(int value)
+    {
+        rstnc_wind = value;
+    }
+
 }
+
